76e555e1c32dc78c7c529028413d93f67a76e03b
Shubham Barge <129944430+shubh-200@users.noreply.github.com>
2024-07-01 15:36:15 +0530

    Added exercises and assignments

---
+++ LinkedList_assignment1.java
@@ -0,0 +1,230 @@
+//LL_1
+class Node {
+
+    private String data;
+    private Node next;
+
+    public Node(String data) {
+        this.data = data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public void setNext(Node node) {
+        this.next = node;
+    }
+
+    public String getData() {
+        return this.data;
+    }
+
+    public Node getNext() {
+        return this.next;
+    }
+}
+
+class LinkedList {
+
+    private Node head;
+    private Node tail;
+
+    public Node getHead() {
+        return this.head;
+    }
+
+    public Node getTail() {
+        return this.tail;
+    }
+
+    // Add these setter methods
+    public void setHead(Node head) {
+        this.head = head;
+    }
+
+    public void setTail(Node tail) {
+        this.tail = tail;
+    }
+
+    public void addAtEnd(String data) {
+        Node node = new Node(data);
+
+        if (this.head == null) {
+            this.head = this.tail = node;
+        } else {
+            this.tail.setNext(node);
+            this.tail = node;
+        }
+    }
+
+    public void addAtBeginning(String data) {
+        Node node = new Node(data);
+
+        if (this.head == null) {
+            this.head = this.tail = node;
+        } else {
+            node.setNext(this.head);
+            this.head = node;
+        }
+    }
+
+    public void display() {
+        Node temp = this.head;
+
+        while (temp != null) {
+            System.out.println(temp.getData());
+            temp = temp.getNext();
+        }
+    }
+
+    public Node find(String data) {
+        Node temp = this.head;
+
+        while (temp != null) {
+            if (temp.getData().equals(data))
+                return temp;
+            temp = temp.getNext();
+        }
+        return null;
+    }
+
+    public void insert(String data, String dataBefore) {
+        Node node = new Node(data);
+
+        if (this.head == null)
+            this.head = this.tail = node;
+        else {
+            Node nodeBefore = this.find(dataBefore);
+            if (nodeBefore != null) {
+                node.setNext(nodeBefore.getNext());
+                nodeBefore.setNext(node);
+                if (nodeBefore == this.tail)
+                    this.tail = node;
+            } else
+                System.out.println("Node not found");
+        }
+    }
+
+    public void delete(String data) {
+        if (this.head == null) {
+            System.out.println("List is empty");
+            return;
+        }
+
+        Node node = this.find(data);
+        if (node == null) {
+            System.out.println("Node not found");
+            return;
+        }
+
+        if (node == this.head) {
+            this.head = this.head.getNext();
+            node.setNext(null);
+            if (node == this.tail)
+                tail = null;
+        } else {
+            Node nodeBefore = null;
+            Node temp = this.head;
+            while (temp != null) {
+                if (temp.getNext() == node) {
+                    nodeBefore = temp;
+                    break;
+                }
+                temp = temp.getNext();
+            }
+
+            if (nodeBefore != null) {
+                nodeBefore.setNext(node.getNext());
+                if (node == this.tail)
+                    this.tail = nodeBefore;
+                node.setNext(null);
+            }
+        }
+    }
+}
+
+class Tester {
+
+    public static void main(String args[]) {
+        LinkedList linkedList1 = new LinkedList();
+        linkedList1.addAtEnd("ABC");
+        linkedList1.addAtEnd("DFG");
+        linkedList1.addAtEnd("XYZ");
+        linkedList1.addAtEnd("EFG");
+
+        LinkedList linkedList2 = new LinkedList();
+        linkedList2.addAtEnd("ABC");
+        linkedList2.addAtEnd("DFG");
+        linkedList2.addAtEnd("XYZ");
+        linkedList2.addAtEnd("EFG");
+
+        System.out.println("Initial List");
+        linkedList1.display();
+
+        System.out.println("\nList after left shifting by 2 positions");
+        shiftListLeft(linkedList1, 2);
+        linkedList1.display();
+
+        System.out.println("\nInitial List");
+        linkedList2.display();
+
+        System.out.println("\nList after right shifting by 2 positions");
+        shiftListRight(linkedList2, 2);
+        linkedList2.display();
+    }
+
+    public static void shiftListLeft(LinkedList linkedList, int n) {
+        if (linkedList.getHead() == null || n <= 0) {
+            return;
+        }
+
+        Node current = linkedList.getHead();
+        Node prev = null;
+        int count = 0;
+
+        // Find the nth node
+        while (current != null && count < n) {
+            prev = current;
+            current = current.getNext();
+            count++;
+        }
+
+        if (current == null) {
+            return; // n is greater than the size of the list
+        }
+
+        Node newHead = current;
+        Node oldTail = linkedList.getTail();
+
+        // Make the current tail's next point to the old head
+        oldTail.setNext(linkedList.getHead());
+
+        // Set the new head and tail
+        linkedList.setHead(newHead);
+        if (prev != null) {
+            prev.setNext(null);
+        }
+        linkedList.setTail(prev);
+    }
+
+    public static void shiftListRight(LinkedList linkedList, int n) {
+        if (linkedList.getHead() == null || n <= 0) {
+            return;
+        }
+
+        // Calculate the size of the linked list
+        Node current = linkedList.getHead();
+        int size = 0;
+        while (current != null) {
+            size++;
+            current = current.getNext();
+        }
+
+        // Calculate effective left shift
+        int leftShift = size - n % size;
+
+        // Perform left shift
+        shiftListLeft(linkedList, leftShift);
+    }
+}
